51184007ff419db17f5a5a3a476e8605aa9481c9
fahimz7 <fahim.zaman7@gmail.com>
2020-01-16 23:28:15 -0500

    Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,17 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
-
+    for (int i=0; i<40; i++) {
+        System.out.print(fNum(i)+",");
+    }
 
     }
+
+    public static int fNum(int z) {
+        if (z<=1)
+            return z;
+        return fNum(z-1) +fNum(z-2);
+
+    }
+
 }
