66ef92e085dd62d30b47b240a6a195f459301de7
fahimz7 <fahim.zaman7@gmail.com>
2020-01-17 01:26:32 -0500

    duplicateWord

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,6 +1,9 @@
 package string.problems;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -10,7 +13,29 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        st = st.toLowerCase();
+        HashMap<String, Integer> jProgram = new HashMap<>();
+        for (String word : st.split(" ")) {
 
+            int i = jProgram.getOrDefault(word, 0);
+            jProgram.put(word, i + 1);
+        }
+
+        ArrayList<Integer> lengths = new ArrayList<>();
+        System.out.println("Duplicate words and occurrences:");
+        for (String key : jProgram.keySet()) {
+
+            lengths.add(key.length());
+            if (jProgram.get(key) > 1)
+                System.out.println("The word "+key +" "+ "duplicated"+" " + jProgram.get(key)+" times");
+
+        }
+        Integer sum = 0;
+        for (Integer length : lengths) {
+            sum += length;
+        }
+        int average = sum / lengths.size();
+
+        System.out.println("Average length of the words is: " + average+" letters");
     }
-
 }
