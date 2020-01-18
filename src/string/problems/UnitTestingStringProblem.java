4df9c744ced5a05e488125b8e6bb9dd311788ad5
fahimz7 <fahim.zaman7@gmail.com>
2020-01-17 23:41:15 -0500

    primeNum

--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -1,8 +1,40 @@
 package string.problems;
 
 
-public class UnitTestingStringProblem {
+import org.testng.annotations.Test;
 
-        //Apply Unit Test into all the methods in this package.
+import java.util.ArrayList;
+
+import static org.testng.AssertJUnit.*;
+
+public class UnitTestingStringProblem {
+    //Apply Unit Test into all the methods in this package.
+
+    @Test
+    public void testAnagram() {
+        System.out.println("Testing isAnagram method:");
+        assertTrue(Anagram.isAnagram("Debit card", "Bad credit"));
+        assertTrue(Anagram.isAnagram("School master", "The classroom"));
+        assertFalse(Anagram.isAnagram("Race car", "Cars race"));
+    }
+
+    @Test
+    public void testisPalindrome() {
+
+        ArrayList<String> truePalindromes = new ArrayList<>();
+        ArrayList<String> falsePalindromes = new ArrayList<>();
+        truePalindromes.add("Mom");
+        truePalindromes.add("Dad");
+        truePalindromes.add("Noon");
+        falsePalindromes.add("Sister");
+        falsePalindromes.add("Moon");
+
+        System.out.println("Testing isPalindrome method:");
+        for(String s: truePalindromes)
+            assertTrue(Palindrome.isPalindrome(s));
+        for(String s: falsePalindromes)
+            assertFalse(Palindrome.isPalindrome(s));
 
     }
+
+}
